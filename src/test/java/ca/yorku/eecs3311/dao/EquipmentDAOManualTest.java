aa1b3203bdc185a93e3b29e0d448b014225a979c
egeyesss <egeyesilyurtca@gmail.com>
2026-03-29 16:13:26 -0400

    Phase 1 of Deliverable 3

---
+++ src/test/java/ca/yorku/eecs3311/dao/EquipmentDAOManualTest.java
@@ -0,0 +1,13 @@
+package ca.yorku.eecs3311.dao;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EquipmentDAOManualTest {
+
+    @Test
+    public void testDaoPlaceholder() {
+        // Placeholder for the team member assigned to test the CSV read/write logic
+        assertTrue(true);
+    }
+}
